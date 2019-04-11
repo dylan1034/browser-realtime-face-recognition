2b325e36fdaeb4e5813e3545d064253e1ded19f0
Jacen-Chou <a774845313@163.com>
2019-04-11 21:58:48 +0800

    Updates

---
+++ precache-manifest.d6cc589e06f4c41303e29768daa71456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/browser-realtime-face-recognition/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "3239acafcf310200789b",
+    "url": "/browser-realtime-face-recognition/static/js/runtime~main.3239acaf.js"
+  },
+  {
+    "revision": "9b89bd4dfafff50bb20f",
+    "url": "/browser-realtime-face-recognition/static/js/main.9b89bd4d.chunk.js"
+  },
+  {
+    "revision": "9291074adbbf51e904ec",
+    "url": "/browser-realtime-face-recognition/static/js/1.9291074a.chunk.js"
+  },
+  {
+    "revision": "9b89bd4dfafff50bb20f",
+    "url": "/browser-realtime-face-recognition/static/css/main.23b4575f.chunk.css"
+  },
+  {
+    "revision": "9291074adbbf51e904ec",
+    "url": "/browser-realtime-face-recognition/static/css/1.ae1e8faa.chunk.css"
+  },
+  {
+    "revision": "521ccfe3a9df3a038ac5d4278f52ad68",
+    "url": "/browser-realtime-face-recognition/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/browser-realtime-face-recognition/precache-manifest.ce8dbe605114def66f2559827e04c10f.js"
+  "/browser-realtime-face-recognition/precache-manifest.d6cc589e06f4c41303e29768daa71456.js"
 );
 
 workbox.clientsClaim();
